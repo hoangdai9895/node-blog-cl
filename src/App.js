a343e6601401444f6934a0dc9d349f4fd6c017e3
unknown <hoangdai9895@gmail.com>
2019-04-11 17:09:06 +0700

    10

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
+        <BrowserRouter>
           <div className="App">
             <div className="container fixed-top px-0">
               <NavBar />
@@ -110,7 +110,7 @@ class App extends Component {
               <Footer />
             </div>
           </div>
-        </Router>
+        </BrowserRouter>
       </Provider>
     );
   }
