d37ca9186f529a9d23d0e73f179be6d545ef50f6
unknown <hoangdai9895@gmail.com>
2019-04-11 14:35:15 +0700

    Updates

---
+++ precache-manifest.2254366c7042ee4c8d39086d7cd13fab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6ec563bcc7bfe131ea1976cb17b4915",
+    "url": "/node-blog-cl/static/media/loading.a6ec563b.gif"
+  },
+  {
+    "revision": "1e914f2cd9dfa16fc5d2718acb6e9477",
+    "url": "/node-blog-cl/static/media/landing1.1e914f2c.jpg"
+  },
+  {
+    "revision": "ce0507d59b4ac1a50983",
+    "url": "/node-blog-cl/static/js/runtime~main.7d7c4e48.js"
+  },
+  {
+    "revision": "682f93a1cca8675b4252",
+    "url": "/node-blog-cl/static/js/main.9c666c66.chunk.js"
+  },
+  {
+    "revision": "49981ba1d27495e7d701",
+    "url": "/node-blog-cl/static/js/2.4f90f9da.chunk.js"
+  },
+  {
+    "revision": "682f93a1cca8675b4252",
+    "url": "/node-blog-cl/static/css/main.cb8d2955.chunk.css"
+  },
+  {
+    "revision": "6b1bfaca7d09ea7f658bb75b703fc09b",
+    "url": "/node-blog-cl/index.html"
+  }
+];
